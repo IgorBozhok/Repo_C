059b4b7a1a2d148a185d744ff72cd53879214592
IgorBozhok <31074368+IgorBozhok@users.noreply.github.com>
2017-09-05 21:20:19 +0300

    Add files via upload

--- Studies/05.09/05.09.c
+++ Studies/05.09/05.09.c
@@ -4,15 +4,16 @@
 #include <locale.h>
 #include <malloc.h>
 #include <stdbool.h>
+#include <stdint.h>
 
-void initArr(int[][5], unsigned, unsigned);
-void printArr(int[][5], unsigned, unsigned);
+void initArr(int[][5], uint32_t, uint32_t);
+void printArr(int[][5], uint32_t, uint32_t);
 
 
 int main()
 {
 	setlocale(LC_ALL, "rus");
-	srand((unsigned)time(NULL));
+	srand((uint32_t)time(NULL));
 	int Arr[4][5];
 	initArr(Arr, 4, 5);
 	printArr(Arr, 4, 5);
@@ -20,19 +21,19 @@ int main()
 	return 0;
 }
 
-void initArr(int arr[][5], unsigned line, unsigned column)
+void initArr(int arr[][5], uint32_t M, uint32_t N)
 {
-	for (int i = 0; i < line; ++i) {
-		for (int j = 0; j < column; ++j) {
+	for (int i = 0; i < M; ++i) {
+		for (int j = 0; j < N; ++j) {
 			arr[i][j] = 1 + rand() % 100;
 		}
 	}
 }
-void printArr(int arr[][5], unsigned line,unsigned column)
+void printArr(int arr[][5], uint32_t M, uint32_t N)
 {
-	for (int i = 0; i < line; ++i) {
-		for (int j = 0; j < column; ++j) {
-			printf_s("Arr[%d][%d]=%d\n ",i,j, arr[i][j]);
+	for (int i = 0; i < M; ++i) {
+		for (int j = 0; j < N; ++j) {
+			printf_s(" Arr[%d][%d]=%d\n ",i,j, arr[i][j]);
 		}
 	}
 }
