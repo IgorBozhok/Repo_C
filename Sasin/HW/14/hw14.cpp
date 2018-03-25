ac423f879e0da6d8c7842b0f5e22d11539a1b8aa
IgorBozhok <31074368+IgorBozhok@users.noreply.github.com>
2018-03-25 23:30:39 +0300

    Add files via upload

---
+++ Sasin/HW/14/hw14.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+#include <iostream>
+
+using namespace std;
+
+void main(void)
+{
+	setlocale(LC_ALL, "Russian");
+	cout << "Отдохнул" << endl;
+	_getch();
+	return;
+}
