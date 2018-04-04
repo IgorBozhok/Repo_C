b3b61a55cb13d5811e89bdb6a3cacbf663f401c9
IgorBozhok <31074368+IgorBozhok@users.noreply.github.com>
2018-04-04 18:00:53 +0300

    Add files via upload

---
+++ Sasin/HW/24/hw24.cpp
@@ -0,0 +1,17 @@
+#include <conio.h>
+#include <time.h>
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+void Hello()
+{
+	cout << "Hello, world!";
+}
+
+void main()
+{
+	Hello();
+	_getch();
+	return;
+}
