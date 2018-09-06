3ffc469407cdc25d3d4d4596a1ef34193193ac51
IgorBozhok <31074368+IgorBozhok@users.noreply.github.com>
2018-09-06 21:11:37 +0300

    Add files via upload

---
+++ C/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DemoClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Server.providerName = "BELTELECOM";
+
+            Server server = new Server();
+            Server server2 = new Server();
+
+            server.SetProvider();
+
+            Console.Read();
+        }
+    }
+}
---
+++ C/Server.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+
+namespace DemoClasses
+{
+    internal class Server
+    {
+        public static string providerName;
+
+        private const int DEFAULT_COUNT_PROCESS = 5;
+        private string type; // field(поля)
+        private int countProcess; // field(поля)
+
+        public Server():this(String.Empty , DEFAULT_COUNT_PROCESS)
+        {
+
+        }
+
+        public Server(string type, int countProcess)
+        {
+            this.type = type;
+            this.countProcess = countProcess;
+        }
+
+        public Server(string type):this(type, DEFAULT_COUNT_PROCESS)
+        {
+
+        }
+       
+        static Server()
+        {
+            providerName = "BELTELECOM";
+        }
+
+        public static void SetProvider(string providerName)
+        {
+            providerName = "By Fly";
+        }
+
+        public void ShowInfo()
+        {
+
+            Console.WriteLine(type);
+            Console.WriteLine(countProcess);
+        }
+
+        public void SetProvider()
+        {
+            providerName = "By Fly";
+        }
+    }
+
+}
