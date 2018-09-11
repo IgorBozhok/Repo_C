9f324dce7e21501ca754c7572c45d8cba6847d74
IgorBozhok <31074368+IgorBozhok@users.noreply.github.com>
2018-09-11 23:26:08 +0300

    Add files via upload

---
+++ C/11.09/hw/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace CW1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SmartArray arr = new SmartArray(10, -10);
+
+            arr[-9] = 10;
+            for (int i = arr.FerstIndex; i < arr.LastIndex; i++)
+            {
+                Console.Write("{0}" + " ", arr[i]);
+            }
+           
+            Console.ReadKey();
+        }
+
+
+    }
+}
---
+++ C/11.09/hw/SmartArray.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+
+namespace CW1
+{
+    internal class SmartArray
+    {
+        private int[] array;
+        int ferstIndex;
+        int lastIndex;
+
+        public SmartArray(int sizeArr, int ferstIndex)
+        {
+            if (sizeArr <= 0)
+            {
+                Console.WriteLine("Ошибка!! sizeArr <= 0 ");
+            }
+            this.ferstIndex = ferstIndex;
+            array = new int[sizeArr];
+            this.lastIndex = this.ferstIndex + array.Length;
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+
+                int tmpIndex = index - this.ferstIndex;
+                return array[tmpIndex];
+            }
+            set
+            {
+                int tmpIndex = index - this.ferstIndex;
+                array[tmpIndex] = value;
+            }
+        }
+
+        public int LenArray
+        {
+            get { return array.Length; }
+        }
+        public int FerstIndex
+        {
+            get { return  ferstIndex; }
+        }
+        public int LastIndex
+        {
+            get { return lastIndex; }
+        }
+
+    }
+}
