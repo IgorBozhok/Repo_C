ed242fa147edfbd39c24b74ce13b65a5c60b9f6e
IgorBozhok <31074368+IgorBozhok@users.noreply.github.com>
2018-09-11 21:21:41 +0300

    Add files via upload

---
+++ C/11.09/Array.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+namespace CW1
+{
+     internal class SmartArray
+    {
+        private int[] array;
+        int ferstIndex;
+        int lastIndex;
+
+        public SmartArray(int sizeArr, int ferstIndex)
+        {
+           if (sizeArr <= 0)
+            {
+                Console.WriteLine("Ошибка!! sizeArr <= 0 ");
+            }
+            this.ferstIndex = ferstIndex;
+            
+            array = new int[sizeArr];
+            this.lastIndex = this.ferstIndex + array.Length-1;
+        }
+        public int this[int index]
+        {
+            get
+            {
+
+                int tmpIndex = lastIndex - index;
+                return array[tmpIndex];
+            }
+            set
+            {
+                int tmpIndex = lastIndex - index;
+                array[tmpIndex] = value;
+            }
+        }
+
+
+    }
+}
---
+++ C/11.09/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace CW1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SmartArray arr = new SmartArray(4,-2);
+            arr[-1] = 10;
+            Console.WriteLine("{0}", arr[-1] );
+            Console.ReadKey();
+        }
+
+        
+    }
+}
